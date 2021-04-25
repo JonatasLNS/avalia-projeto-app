54bc33c6654a6ef861bb0acd3bb206cfcfed7b39
Jonatas Lucas <jonataslns@gmail.com>
2021-04-24 23:46:43 -0300

    Criação do service de professor

---
+++ src/app/service/professorService.js
@@ -0,0 +1,22 @@
+import ApiService from '../apiservice'
+
+export default class AlunosService extends ApiService {
+    constructor(){
+        // Precisa ser igual ao @RequestMapping da classe Resource
+        super('/api/professores')
+    }
+
+    consultar(professorFiltro){
+        let params = `/?`
+
+        if(professorFiltro.nome){
+            params = `${params}&nome=${professorFiltro.nome}`
+        }
+
+        if(professorFiltro.disciplina){
+            params = `${params}&disciplina=${professorFiltro.disciplina}`
+        }
+
+        return this.get(params)
+    }
+}
